d91f9b2b60a0322af72d4a5ebd2da3eb307dd482
EL-CHAMAA OMAR <chamaael@etu.unistra.fr>
2023-02-12 18:28:27 +0100

    Class Creation

---
+++ include/powerUp.h
@@ -0,0 +1,14 @@
+//
+// Created by omar on 12/02/23.
+//
+
+#ifndef PEC_MEN_POWERUP_H
+#define PEC_MEN_POWERUP_H
+
+
+class powerUp {
+
+};
+
+
+#endif //PEC_MEN_POWERUP_H
---
+++ src/powerUp.cpp
@@ -0,0 +1,5 @@
+//
+// Created by omar on 12/02/23.
+//
+
+#include "powerUp.h"
