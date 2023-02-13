79072694eb5526507f02f55d117d34da60b78fec
EL-CHAMAA OMAR <chamaael@etu.unistra.fr>
2023-02-13 20:01:46 +0100

    Added class headers

--- include/powerUp.h
+++ include/powerUp.h
@@ -4,9 +4,32 @@
 
 #ifndef PEC_MEN_POWERUP_H
 #define PEC_MEN_POWERUP_H
+#include <SDL.h>
+#include <utility>
 #include "entity.h"
+#include "constants.h"
 
-class powerUp : public Entity{
+class PowerUp : public Entity{
+
+protected:
+    powerUps type ;
+    SDL_Rect sprite;
+public:
+
+    PowerUp();
+
+    PowerUp(const powerUps &type, const SDL_Rect &sprite);
+
+    [[nodiscard]] const powerUps &getType() const;
+
+    void setType(const powerUps type);
+
+    [[nodiscard]] const SDL_Rect &getSprite() const;
+
+    void setSprite(const SDL_Rect sprite);
+
+    virtual ~PowerUp();
+
 
 };
 
