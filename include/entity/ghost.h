56829d9d3207c2d801b2c04b1139ed3b24e97520
mfreitag <mfreitag@numericable.fr>
2023-04-03 11:29:56 +0200

    prepare base ghost class

--- include/entity/ghost.h
+++ include/entity/ghost.h
@@ -1,103 +1,41 @@
-//
-// Created by omar on 07/02/23.
-//
+/**
+ * @file ghost.h
+ * @brief Defines the Ghost class, which is the second main entity in the game.
+ * @author Matthieu FREITAG (Zapharaos)
+ * @date 03/04/2023
+*/
 
 #ifndef PEC_MEN_GHOST_H
 #define PEC_MEN_GHOST_H
 
-#include <SDL.h>
+#include "movingEntity.h"
 
-#include <string>
-#include <iostream>
-#include <utility>
-
-#include "entity.h"
-
-class Ghost : public Entity
+/**
+ * @brief The Ghost entity.
+ * Inherits from the MovingEntity class.
+ * @see MovingEntity
+*/
+class Ghost : public MovingEntity
 {
 
 private:
-    // all sprites
-    SDL_Rect right;
-    SDL_Rect left;
-    SDL_Rect down;
-    SDL_Rect up;
-    SDL_Rect scared;
-    SDL_Rect less_scared;
-    SDL_Rect invis_up;
-    SDL_Rect invis_down;
-    SDL_Rect invis_right;
-    SDL_Rect invis_left;
 
-    //starting position on pacman_map.txt
-    SDL_Rect starting;     // ici scale x2
-
-    int state;
-
-    // Ghost states
-    enum ghost_states
+    enum class GhostStatus
     {
-        kDefault = 0,
-        kScared = 1,
-        kLessScared = 2,
-        kInvisible = 3
+        kDefault,
+        kScared,
+        kLessScared,
+        kInvisible
     };
+
+    GhostStatus status_ {GhostStatus::kDefault};
 
 public:
 
-    Ghost(const SDL_Rect &right, const SDL_Rect &left, const SDL_Rect &down,
-          const SDL_Rect &up, const SDL_Rect &scared,
-          const SDL_Rect &lessScared, const SDL_Rect &invisUp,
-          const SDL_Rect &invisDown, const SDL_Rect &invisRight,
-          const SDL_Rect &invisLeft, const SDL_Rect &starting, int state);
-
-    [[nodiscard]] const SDL_Rect &getRight() const;
-
-    void setRight(const SDL_Rect &right);
-
-    [[nodiscard]] const SDL_Rect &getLeft() const;
-
-    void setLeft(const SDL_Rect &left);
-
-    [[nodiscard]] const SDL_Rect &getDown() const;
-
-    void setDown(const SDL_Rect &down);
-
-    [[nodiscard]] const SDL_Rect &getUp() const;
-
-    void setUp(const SDL_Rect &up);
-
-    [[nodiscard]] const SDL_Rect &getScared() const;
-
-    void setScared(const SDL_Rect &scared);
-
-    [[nodiscard]] const SDL_Rect &getLessScared() const;
-
-    void setLessScared(const SDL_Rect &lessScared);
-
-    [[nodiscard]] const SDL_Rect &getInvisUp() const;
-
-    void setInvisUp(const SDL_Rect &invisUp);
-
-    [[nodiscard]] const SDL_Rect &getInvisDown() const;
-
-    void setInvisDown(const SDL_Rect &invisDown);
-
-    [[nodiscard]] const SDL_Rect &getInvisRight() const;
-
-    void setInvisRight(const SDL_Rect &invisRight);
-
-    [[nodiscard]] const SDL_Rect &getInvisLeft() const;
-
-    void setInvisLeft(const SDL_Rect &invisLeft);
-
-    [[nodiscard]] const SDL_Rect &getStarting() const;
-
-    void setStarting(const SDL_Rect &starting);
-
-    [[nodiscard]] int getState() const;
-
-    void setState(int state);
+    /**
+     * @brief Default Ghost constructor.
+     */
+    Ghost();
 
 };
 
